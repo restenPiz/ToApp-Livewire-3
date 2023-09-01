1bb747e747dd055402f49cacb61ed397ba537885
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-01 23:58:49 +0200

    Some updates in my application

--- todoApp_livewire3-app/resources/views/livewire/layouts/navbar.blade.php
+++ todoApp_livewire3-app/resources/views/livewire/layouts/navbar.blade.php
@@ -1,5 +1,8 @@
 <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
         <span class="navbar-brand mb-0 h1">Navbar</span>
+        <div class="div">
+            <h3>TodoApp</h3>
+        </div>
     </div>
 </nav>
--- todoApp_livewire3-app/resources/views/livewire/task-controller.blade.php
+++ todoApp_livewire3-app/resources/views/livewire/task-controller.blade.php
@@ -3,23 +3,44 @@
     {{--Inicio do navbar--}}
     @include('livewire.layouts.navbar')
 
-    {{--Inicio do titulo da applicacao--}}
-    <h1 style="text-align: center">Todo App</h1>
-
     {{--Inicio do formulario de submissao--}}
-    <div class="container">
-        <form action="">
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                </div>
-                <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <div class="container" style="margin-top:3rem">
+        <div class="card">
+            <div class="container">
+                <form action="">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Tarefa</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Digite a sua tarefa">
+                    </div>
+        
+                    {{--Inicio do butao submit--}}
+                    <button type="button" class="btn btn-success">Adicionar Tarefa</button>
+                </form><br>
             </div>
-        </form>
+        </div>
+    </div>
+
+    {{--Inicio do titulo--}}
+    <div class="container" style="margin-top:5rem">
+        <h1>Todas Tarefas</h1>
     </div>
 
     {{--Inicio da tabela responsavel por listar os dados--}}
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Tarefa</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+              </tr>
+            </tbody>
+        </table>
+    </div>
 
 </div>
