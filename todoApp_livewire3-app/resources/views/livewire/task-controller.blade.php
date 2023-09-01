5626897fcecdb6f4ee46d206f285414299f7c9ec
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-01 23:46:26 +0200

    Renderizing my component

--- todoApp_livewire3-app/resources/views/livewire/task-controller.blade.php
+++ todoApp_livewire3-app/resources/views/livewire/task-controller.blade.php
@@ -7,5 +7,21 @@
         </div>
     </nav>
 
+    {{--Inicio do titulo da applicacao--}}
+    <h1 style="text-align: center">Todo App</h1>
+
+    {{--Inicio do formulario de submissao--}}
+    <div class="container">
+        <form action="">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </form>
+    </div>
 
 </div>
