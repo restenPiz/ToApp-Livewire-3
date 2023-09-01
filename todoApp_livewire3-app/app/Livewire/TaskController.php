dcff6b8aacdcad2bebffd3868c6923a5e7c23422
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-02 00:27:31 +0200

    Some updates in taskController

--- todoApp_livewire3-app/app/Livewire/TaskController.php
+++ todoApp_livewire3-app/app/Livewire/TaskController.php
@@ -2,15 +2,37 @@
 
 namespace App\Livewire;
 
+use Illuminate\Console\View\Components\Task;
 use Livewire\Component;
+use Request;
 
 class TaskController extends Component
 {
+    //*Declarando as variaveis que irei usar na aplicacao
+    public $task;
+
     //?Inicio do metodo principal responsavel pela pagina principal
     public function render()
     {
-        return view('livewire.task-controller');
+        $tasks=Task::all();
+
+        return view('livewire.task-controller',compact('tasks'));
     }
 
     //?Inicio do metodo para salvar os dados
+    public function save()
+    {
+        $table=new \App\Models\Task();
+
+        $this->validate([
+            'Task'=>'required',
+        ]);
+
+        $table=Request::input($this->task);
+
+        $table->save();
+
+        return back();
+
+    }
 }
