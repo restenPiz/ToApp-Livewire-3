89cb589b4bd2fa554ade8a400d24c110db7b5f9e
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-03 00:37:19 +0200

    Some updates in my application

--- todoApp_livewire3-app/resources/views/livewire/task-controller.blade.php
+++ todoApp_livewire3-app/resources/views/livewire/task-controller.blade.php
@@ -70,27 +70,6 @@
                 </div>
                 @endif
 
-                {{--Inicio do modal de eliminar--}}
-                @if($showDeleteModal)
-                <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLabel">Confirmar Exclusão</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Tem certeza de que deseja excluir esta tarefa?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                                <button wire:click="delete" class="btn btn-danger">Excluir</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endif
-
               @endforeach
             </tbody>
         </table>
