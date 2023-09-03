7ff006945cbdc65ad8587e5875e8a80af1947054
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-04 01:08:41 +0200

    Some updates in my application

--- todoApp_livewire3-app/resources/views/livewire/layouts/navbar.blade.php
+++ todoApp_livewire3-app/resources/views/livewire/layouts/navbar.blade.php
@@ -1,8 +1,10 @@
-<nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Navbar</span>
-        <div class="div">
-            <h3>TodoApp</h3>
+<div class="container">
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+            <div class="div">
+                <h3>TodoApp</h3>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
